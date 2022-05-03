03d4136df0a78dc57600858ab0095790df9692d7
윤동희 <yundonghui@yundonghuiui-MacBookPro.local>
2022-05-03 23:03:27 +0900

    first

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const addrFrom = '0.0.0.0';
+
+app.get("/", (req,res) => {
+    res.send("hello i'm nodejs app v1!!")
+})
+app.listen(port, addrFrom, () => {
+    console.log('Broker API is listening on port'+port);
+});
