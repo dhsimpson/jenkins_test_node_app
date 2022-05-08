27348d64ea3a4d6618d3c9abc6db9f9e2bad8329
dhsimpson <qeadzc9503@naver.com>
2022-05-09 02:32:49 +0900

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000;
 const addrFrom = '0.0.0.0';
 
 app.get("/", (req,res) => {
-    res.send("hello i'm nodejs app v2!!")
+    res.send("hello i'm nodejs app v3!!")
 })
 app.listen(port, addrFrom, () => {
     console.log('Broker API is listening on port'+port);
